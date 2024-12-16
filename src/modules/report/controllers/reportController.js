58c742c2fffbd4e48a0612cb6926195e15ea6b4e
Anwarul Haque <anwarul@b2cinfosolutions.com>
2024-12-16 10:02:57 +0530

    admin client report

--- src/modules/report/controllers/reportController.js
+++ src/modules/report/controllers/reportController.js
@@ -158,3 +158,475 @@ exports.getReport = async (req, res) => {
     }
 
 }
+exports.getClientReport = async (req, res) => {
+
+    try {
+
+        const { startDate, endDate, drowId, clientId } = req.query;
+       
+
+        const startOfDay = new Date(startDate);
+        const endOfDay = new Date(endDate);
+        endOfDay.setDate(endOfDay.getDate() + 1);
+
+
+        const filterData = {
+            clientId: new mongoose.Types.ObjectId(clientId),
+            createdAt: {
+                $gte: startOfDay, // Start of the day
+                $lt: endOfDay     // Start of the next day (exclusive)
+            },
+            ...(drowId && { drowId: new mongoose.Types.ObjectId(drowId) })
+        }
+
+        // console.log(filterData)
+
+
+
+        const report = await Game.aggregate([
+            {
+                $match: filterData
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$drowId",
+                        day: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        gameType: "$gameType",
+                        roundType: "$roundType"
+                    },
+                    passTotalAmount: {
+                        $sum: {
+                            $cond: [{ $eq: ["$status", "PASS"] }, "$amount", 0]
+                        }
+                    },
+                    totalAmount: { $sum: "$amount" },
+                    totalResultAmount: { $sum: "$resultAmount" },
+                    totalClientCommAmount: { $sum: "$clientCommAmount" },
+                    totalAgentCommAmount: { $sum: "$agentCommAmount" }
+                }
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$_id.drowId",
+                        day: "$_id.day"
+                    },
+                    gameTotals: {
+                        $push: {
+                            gameType: "$_id.gameType",
+                            roundType: "$_id.roundType",
+                            totalAmount: "$totalAmount",
+                            totalResultAmount: "$totalResultAmount",
+                            totalClientCommAmount: "$totalClientCommAmount",
+                            totalAgentCommAmount: "$totalAgentCommAmount",
+                            passTotalAmount: "$passTotalAmount",
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$_id.drowId",
+                    days: {
+                        $push: {
+                            day: "$_id.day",
+                            totals: "$gameTotals"
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    drowId: "$_id",
+                    groupedData: {
+                        $arrayToObject: {
+                            $map: {
+                                input: "$days",
+                                as: "dayData",
+                                in: {
+                                    k: "$$dayData.day",
+                                    v: {
+                                        $arrayToObject: {
+                                            $map: {
+                                                input: "$$dayData.totals",
+                                                as: "total",
+                                                in: {
+                                                    k: {
+                                                        $concat: [
+                                                            "$$total.gameType",
+                                                            "_",
+                                                            "$$total.roundType"
+                                                        ]
+                                                    },
+                                                    v: {
+                                                        totalAmount: "$$total.totalAmount",
+                                                        totalResultAmount: "$$total.totalResultAmount",
+                                                        totalClientCommAmount: "$$total.totalClientCommAmount",
+                                                        totalAgentCommAmount: "$$total.totalAgentCommAmount",
+                                                        passTotalAmount: "$$total.passTotalAmount",
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $lookup: {
+                    from: "drows",
+                    localField: "drowId",
+                    foreignField: "_id",
+                    as: "drow",
+                    pipeline: [
+                        {
+                            $project: {
+                                name: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $sort: { drowId: -1 }
+            },
+            {
+                $project: {
+                    groupedData: 1,
+                    drow: { $arrayElemAt: ["$drow", 0] }
+                }
+            }
+        ])
+
+        return res.status(200).json({
+            meta: { msg: `Report found successfully`, status: true },
+            data: report
+        })
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+
+}
+exports.getAgentReport = async (req, res) => {
+
+    try {
+
+        const { startDate, endDate, drowId, agentId } = req.query;
+        const { _id } = req.profile;
+
+        const startOfDay = new Date(startDate);
+        const endOfDay = new Date(endDate);
+        endOfDay.setDate(endOfDay.getDate() + 1);
+
+
+        const filterData = {
+            agentId: new mongoose.Types.ObjectId(agentId),
+            createdAt: {
+                $gte: startOfDay, // Start of the day
+                $lt: endOfDay     // Start of the next day (exclusive)
+            },
+            ...(drowId && { drowId: new mongoose.Types.ObjectId(drowId) })
+        }
+
+        // console.log(filterData)
+
+
+
+        const report = await Game.aggregate([
+            {
+                $match: filterData
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$drowId",
+                        day: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        gameType: "$gameType",
+                        roundType: "$roundType"
+                    },
+                    passTotalAmount: {
+                        $sum: {
+                            $cond: [{ $eq: ["$status", "PASS"] }, "$amount", 0]
+                        }
+                    },
+                    totalAmount: { $sum: "$amount" },
+                    totalResultAmount: { $sum: "$resultAmount" },
+                    totalClientCommAmount: { $sum: "$clientCommAmount" },
+                    totalAgentCommAmount: { $sum: "$agentCommAmount" }
+                }
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$_id.drowId",
+                        day: "$_id.day"
+                    },
+                    gameTotals: {
+                        $push: {
+                            gameType: "$_id.gameType",
+                            roundType: "$_id.roundType",
+                            totalAmount: "$totalAmount",
+                            totalResultAmount: "$totalResultAmount",
+                            totalClientCommAmount: "$totalClientCommAmount",
+                            totalAgentCommAmount: "$totalAgentCommAmount",
+                            passTotalAmount: "$passTotalAmount",
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$_id.drowId",
+                    days: {
+                        $push: {
+                            day: "$_id.day",
+                            totals: "$gameTotals"
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    drowId: "$_id",
+                    groupedData: {
+                        $arrayToObject: {
+                            $map: {
+                                input: "$days",
+                                as: "dayData",
+                                in: {
+                                    k: "$$dayData.day",
+                                    v: {
+                                        $arrayToObject: {
+                                            $map: {
+                                                input: "$$dayData.totals",
+                                                as: "total",
+                                                in: {
+                                                    k: {
+                                                        $concat: [
+                                                            "$$total.gameType",
+                                                            "_",
+                                                            "$$total.roundType"
+                                                        ]
+                                                    },
+                                                    v: {
+                                                        totalAmount: "$$total.totalAmount",
+                                                        totalResultAmount: "$$total.totalResultAmount",
+                                                        totalClientCommAmount: "$$total.totalClientCommAmount",
+                                                        totalAgentCommAmount: "$$total.totalAgentCommAmount",
+                                                        passTotalAmount: "$$total.passTotalAmount",
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $lookup: {
+                    from: "drows",
+                    localField: "drowId",
+                    foreignField: "_id",
+                    as: "drow",
+                    pipeline: [
+                        {
+                            $project: {
+                                name: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $sort: { drowId: -1 }
+            },
+            {
+                $project: {
+                    groupedData: 1,
+                    drow: { $arrayElemAt: ["$drow", 0] }
+                }
+            }
+        ])
+
+        return res.status(200).json({
+            meta: { msg: `Report found successfully`, status: true },
+            data: report
+        })
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+
+}
+
+exports.getAdminReport = async (req, res) => {
+
+    try {
+
+        const { startDate, endDate, drowId } = req.query;
+        const { _id } = req.profile;
+
+        const startOfDay = new Date(startDate);
+        const endOfDay = new Date(endDate);
+        endOfDay.setDate(endOfDay.getDate() + 1);
+
+
+        const filterData = {
+            // clientId: new mongoose.Types.ObjectId(_id),
+            createdAt: {
+                $gte: startOfDay, // Start of the day
+                $lt: endOfDay     // Start of the next day (exclusive)
+            },
+            ...(drowId && { drowId: new mongoose.Types.ObjectId(drowId) })
+        }
+
+        // console.log(filterData)
+
+
+
+        const report = await Game.aggregate([
+            {
+                $match: filterData
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$drowId",
+                        day: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+                        gameType: "$gameType",
+                        roundType: "$roundType"
+                    },
+                    passTotalAmount: {
+                        $sum: {
+                            $cond: [{ $eq: ["$status", "PASS"] }, "$amount", 0]
+                        }
+                    },
+                    totalAmount: { $sum: "$amount" },
+                    totalResultAmount: { $sum: "$resultAmount" },
+                    totalClientCommAmount: { $sum: "$clientCommAmount" },
+                    totalAgentCommAmount: { $sum: "$agentCommAmount" }
+                }
+            },
+
+            {
+                $group: {
+                    _id: {
+                        drowId: "$_id.drowId",
+                        day: "$_id.day"
+                    },
+                    gameTotals: {
+                        $push: {
+                            gameType: "$_id.gameType",
+                            roundType: "$_id.roundType",
+                            totalAmount: "$totalAmount",
+                            totalResultAmount: "$totalResultAmount",
+                            totalClientCommAmount: "$totalClientCommAmount",
+                            totalAgentCommAmount: "$totalAgentCommAmount",
+                            passTotalAmount: "$passTotalAmount",
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$_id.drowId",
+                    days: {
+                        $push: {
+                            day: "$_id.day",
+                            totals: "$gameTotals"
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    drowId: "$_id",
+                    groupedData: {
+                        $arrayToObject: {
+                            $map: {
+                                input: "$days",
+                                as: "dayData",
+                                in: {
+                                    k: "$$dayData.day",
+                                    v: {
+                                        $arrayToObject: {
+                                            $map: {
+                                                input: "$$dayData.totals",
+                                                as: "total",
+                                                in: {
+                                                    k: {
+                                                        $concat: [
+                                                            "$$total.gameType",
+                                                            "_",
+                                                            "$$total.roundType"
+                                                        ]
+                                                    },
+                                                    v: {
+                                                        totalAmount: "$$total.totalAmount",
+                                                        totalResultAmount: "$$total.totalResultAmount",
+                                                        totalClientCommAmount: "$$total.totalClientCommAmount",
+                                                        totalAgentCommAmount: "$$total.totalAgentCommAmount",
+                                                        passTotalAmount: "$$total.passTotalAmount",
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $lookup: {
+                    from: "drows",
+                    localField: "drowId",
+                    foreignField: "_id",
+                    as: "drow",
+                    pipeline: [
+                        {
+                            $project: {
+                                name: 1,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $sort: { drowId: -1 }
+            },
+            {
+                $project: {
+                    groupedData: 1,
+                    drow: { $arrayElemAt: ["$drow", 0] }
+                }
+            }
+        ])
+
+        return res.status(200).json({
+            meta: { msg: `Report found successfully`, status: true },
+            data: report
+        })
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+
+}
--- src/modules/report/routes/reportRoutes.js
+++ src/modules/report/routes/reportRoutes.js
@@ -1,9 +1,12 @@
-const { authClient } = require("../../../middlewares/authMiddleware");
-const { getReport,  } = require("../controllers/reportController");
+const { authClient, authAdmin } = require("../../../middlewares/authMiddleware");
+const { getReport,getClientReport, getAgentReport, getAdminReport  } = require("../controllers/reportController");
 
 const reportRoutes = require("express").Router();
 
 reportRoutes.get("/list", authClient,  getReport);
+reportRoutes.get("/clientReport", authAdmin,  getClientReport);
+reportRoutes.get("/agentReport", authAdmin,  getAgentReport);
+reportRoutes.get("/adminReport", authAdmin,  getAdminReport);
 
 
 module.exports = reportRoutes;
